cd17a74f8c582836cca4b89263d0d738b6cda4ca
Allison <alibellows@gmail.com>
2019-11-11 15:45:29 -0700

    added anaconda env indicator to zsh prompt

--- .p10k.zsh
+++ .p10k.zsh
@@ -49,8 +49,8 @@
       status                  # exit code of the last command
       command_execution_time  # duration of the last command
       background_jobs         # presence of background jobs
+      anaconda                # conda environment (https://conda.io/)
       # virtualenv              # python virtual environment (https://docs.python.org/3/library/venv.html)
-      # anaconda                # conda environment (https://conda.io/)
       # pyenv                   # python environment (https://github.com/pyenv/pyenv)
       # nodenv                  # node.js version from nodenv (https://github.com/nodenv/nodenv)
       # nvm                     # node.js version from nvm (https://github.com/nvm-sh/nvm)
